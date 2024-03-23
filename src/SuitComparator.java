022a03cbfe1f87c2fca37357717af9ff5ef52429
AlligatorBl00d <125635401+AlligatorBl00d@users.noreply.github.com>
2024-03-23 15:41:54 +0800

    Update SuitComparator.java
    add in docs

--- src/SuitComparator.java
+++ src/SuitComparator.java
@@ -1,6 +1,17 @@
 import java.util.Comparator;
+/**
+ * Comparator for comparing cards based on their suits.
+ */
 
 public class SuitComparator implements Comparator<Card> {
+     /**
+     * Compares two cards based on their ranks.
+     *
+     * @param c1 the first card to be compared
+     * @param c2 the second card to be compared
+     * @return a negative integer, zero, or a positive integer as the first card 
+     *         is less than, equal to, or greater than the second card based on suit
+     */
     @Override
     public int compare(Card c1, Card c2) {
         return c1.getSuit().ordinal() - c2.getSuit().ordinal();
