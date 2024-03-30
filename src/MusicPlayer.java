3571ae869fcbae3a709383d14beaec41b46b8a7a
kth <kangtaeho2456@gmail.com>
2024-03-31 00:26:26 +0800

    Update MusicPlayer.java
    Added comments

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -5,10 +5,19 @@ import java.io.FileNotFoundException;
 
 import javax.sound.sampled.*;
 
+/**
+ * A utility class for playing music in the game.
+ */
 public class MusicPlayer {
+    /** The path to the music file. */
     public static final String MUSIC_PATH = "Music/music.wav";
+    
+    /** The audio clip used for playing the music. */
     private static Clip clip;
 
+    /**
+     * Plays the music.
+     */
     public static void playMusic() {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(MUSIC_PATH));
@@ -26,10 +35,16 @@ public class MusicPlayer {
         }
     }
 
+    /**
+     * Stops the music.
+     */
     public static void stopMusic() {
         clip.stop();
     }
 
+    /**
+     * Plays or stops the music based on its current state.
+     */
     public static void playOrStopMusic() {
         if (clip != null && clip.isRunning()) {
             stopMusic();
