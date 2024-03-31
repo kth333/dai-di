463e1c260320559026ba022324b2ddb8c162a6f2
kth <kangtaeho2456@gmail.com>
2024-03-31 16:50:05 +0800

    Update App.java
    all caps

--- src/App.java
+++ src/App.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
  * The main application class for running the Dai Di game.
  */
 class App {
-    private static final Scanner scanner = new Scanner(System.in);
-    private final Game game;
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private final Game GAME;
 
     /**
      * Constructs an instance of the App class with a specified Game object.
      * 
-     * @param game the Game object to associate with this application
+     * @param GAME the Game object to associate with this application
      */
-    public App(Game game) {
-        this.game = game;
+    public App(Game GAME) {
+        this.GAME = GAME;
     }
 
     /**
@@ -27,7 +27,7 @@ class App {
         // Start playing music
         MusicPlayer.playMusic();
 
-        String playerName = GameView.getName("Enter your name (up to 16 characters): ", scanner);
+        String playerName = GameView.getName("Enter your name (up to 16 characters): ", SCANNER);
 
         while (true) {
             // Display the main menu options
@@ -37,11 +37,11 @@ class App {
             System.out.println("3. Quit game");
             //System.out.print("\nChoose an option: ");
             // Read the user's choice
-            int choice = GameView.getInt("\nChoose an option: ",scanner);
+            int choice = GameView.getInt("\nChoose an option: ",SCANNER);
             switch (choice) {
                 case 1:
                     // Start the game with the entered player name
-                    game.startGame(playerName, scanner);
+                    GAME.startGame(playerName, SCANNER);
                     return;
                 case 2:
                     // Display game instructions
